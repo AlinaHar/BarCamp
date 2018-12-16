e72d4339d6eb0e1e2fe2820d4afa13a2ae06f7ae
Alina Harutyunyan <alinaharutyunyants@gmail.com>
2018-12-16 19:04:45 +0400

    initial commit

--- app/src/main/java/com/smartcamp/aua/loginregister/RetreiveWorkshopList.java
+++ app/src/main/java/com/smartcamp/aua/loginregister/RetreiveWorkshopList.java
@@ -1,6 +1,8 @@
 package com.smartcamp.aua.loginregister;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,18 +11,52 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class WorkshopListActivity extends AppCompatActivity {
+public class RetreiveWorkshopList extends AppCompatActivity {
+
+    private ListView lvWorkshopList;//layout
+    private DatabaseReference dbRef;//firebase reference
+    private ArrayList<String> arrayList = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workshop_list);
 
-        final ListView lvWorkshopList = (ListView) findViewById(R.id.lvWorkshopList);
-        String[] array = {"Workshop1", "Workshop2","Workshop3", "Workshop4","Workshop5", "Workshop6",
+        dbRef = FirebaseDatabase.getInstance().getReference("WorkshopList");
+        lvWorkshopList = (ListView) findViewById(R.id.lvWorkshopList);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+
+        lvWorkshopList.setAdapter(adapter);
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //to ensure that all children are successfully accessed(workshop1, workshop2, etc)
+                for(DataSnapshot ds:dataSnapshot.getChildren()){
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+        /*String[] array = {"Workshop1", "Workshop2","Workshop3", "Workshop4","Workshop5", "Workshop6",
                             "Workshop7", "Workshop8","Workshop9", "Workshop10","Workshop11", "Workshop12",
                             "Workshop13", "Workshop14","Workshop15", "Workshop16","Workshop17", "Workshop18"};
         //in order be able to update the list dynamically
@@ -44,7 +80,7 @@ public class WorkshopListActivity extends AppCompatActivity {
                 ;
             }
 
-        });
+        });*/
 
 
     }
---
+++ app/src/main/java/com/smartcamp/aua/loginregister/WorkshopList.java
@@ -0,0 +1,4 @@
+package com.smartcamp.aua.loginregister;
+
+public class WorkshopList {
+}
